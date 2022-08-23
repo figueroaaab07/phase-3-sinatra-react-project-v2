69f86aef0162d477271d6fd2986391aea44f9f2f
Alejandro Luis Figueroa Cevallos <figueroaaab07@gmail.com>
2022-08-22 21:22:03 -0700

    Sinatra Backend v2 Production

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # This is an _environment variable_ that is used by some of the Rake tasks to determine
 # if our application is running locally in development, in a test environment, or in production
-ENV['RACK_ENV'] ||= "production"
+ENV['RACK_ENV'] ||= "development"
 
 # Require in Gems
 require 'bundler/setup'
@@ -20,4 +20,5 @@ configure :production do
 end
 
 # Require in all files in 'app' directory
+require_dependency "app/controllers/application_controller"
 require_all 'app'
